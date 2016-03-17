7246f1c1a0cfdd073f4fd29f3427c6495b88d782
dherges <david@spektrakel.de>
2016-03-17 19:56:32 +0100

    js editorconfig

---
+++ editorconfig/.editorconfig.js
@@ -0,0 +1,10 @@
+# Unix-style newlines with a newline ending every file, utf-8 encoded content
+[*]
+end_of_line = lf
+insert_final_newline = true
+charset = utf-8
+
+# JavaScript styles how they're supposed to be :-)
+[*.js]
+indent_style = space
+indent_size = 2
